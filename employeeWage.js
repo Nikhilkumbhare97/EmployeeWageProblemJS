fb018f76875b2f09a3c42e407ee86aa785278f83
nikhilkumbhare <nikhilk190697@gmail.com>
2021-04-12 00:42:16 +0530

    Added: Ability To Perfrom Operations Using Arrow Functions

--- employeeWage.js
+++ employeeWage.js
@@ -10,6 +10,7 @@ let totalEmpHrs = 0;
 let totalWorkingDays = 0;
 let empDailyWageArray = new Array();
 let empDailyWageMap = new Map();
+let empDailyHrsMap = new Map();
 
 while (totalEmpHrs < MAX_WORKING_HRS_IN_MONTH && totalWorkingDays < WORKING_DAYS_PER_MONTH) {
     totalWorkingDays++;
@@ -18,11 +19,12 @@ while (totalEmpHrs < MAX_WORKING_HRS_IN_MONTH && totalWorkingDays < WORKING_DAYS
     totalEmpHrs += empHrs;
     empDailyWageArray.push(calulateDailyWage(empHrs));
     empDailyWageMap.set(totalWorkingDays, calulateDailyWage(empHrs))
+    empDailyHrsMap.set(totalWorkingDays, empHrs);
 }
 // console.log(empDailyWageArray);
 console.log(empDailyWageMap);
 
-//A - Calc Total Wage Using Array for Each or Reduce Method
+//7A - Calc Total Wage Using Array for Each or Reduce Method
 let totEmpWage = 0;
 function sum(dailyWage) {
     totEmpWage += dailyWage;
@@ -36,7 +38,7 @@ function totalWages(totalWage, dailyWage) {
 console.log("Total Employee Wage with Reduce : " + empDailyWageArray.reduce(totalWages, 0));
 console.log("Total Employee Wage using Map : " + Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
 
-//B - Show the Day along with Daily Wage using Array map helper Function
+//7B - Show the Day along with Daily Wage using Array map helper Function
 let dailyCounter = 0;
 function mapDayWithWage(dailyWage) {
     dailyCounter++;
@@ -45,26 +47,26 @@ function mapDayWithWage(dailyWage) {
 let mapWithWageArray = empDailyWageArray.map(mapDayWithWage);
 console.log("Daily Wage Map : " + mapWithWageArray);
 
-//C - Show Days when Full Time wage of 160 were earned using filter Function
+//7C - Show Days when Full Time wage of 160 were earned using filter Function
 function fullTimeWage(dailyWage) {
     return dailyWage.includes("160");
 }
 let fullDayWageArray = mapWithWageArray.filter(fullTimeWage);
 console.log("Daily Wage Filter When FullTime Wage Earned : " + fullDayWageArray);
 
-//D - Find the first Occurence when Full Time Wage was earned using Find Function
+//7D - Find the first Occurence when Full Time Wage was earned using Find Function
 console.log("First time FullTime wage was Earned on a Day : " + mapWithWageArray.find(fullTimeWage));
 
 //E - Check if Every Element of Full Time Wage is truly holding Full time Wage
 console.log("Check All Element have Full Time Wage : " + fullDayWageArray.every(fullTimeWage));
 
-//F - Check if There is any PartTime Wage
+//7F - Check if There is any PartTime Wage
 function isAnyPartTimeWage(dailyWage) {
     return dailyWage.includes("80")
 }
 console.log("Check if Any Part Time Wage : " + mapWithWageArray.some(isAnyPartTimeWage));
 
-//G - Find the Number of Days the Employee Worrked
+//7G - Find the Number of Days the Employee Worrked
 function totalDaysEmpWorked(numOfDays, dailyWage) {
     if (dailyWage > 0) {
         return numOfDays + 1;
@@ -76,6 +78,30 @@ console.log("Number of Days Employee Worked : " + empDailyWageArray.reduce(total
 function calulateDailyWage(empHrs) {
     return empHrs * WAGE_PER_HOUR;
 }
+
+//9A - Calculate Total Wage and Total Hours Worked
+let findTotal = (totalVal, dailyVal) => {
+    return totalVal + dailyVal;
+}
+let count = 0;
+let totalHours = Array.from(empDailyHrsMap.values()).reduce(findTotal, 0);
+let totalSalary = empDailyWageArray.filter(dailyWage => dailyWage > 0).reduce(findTotal, 0);
+console.log("Employee Wage with Arrow => " + " Total Hours => " + totalHours + " Total Wages => " + totalSalary);
+
+//9B - Show the full working days, part working days and no working days
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+
+empDailyHrsMap.forEach((value, key) => {
+    if (value == 8) fullWorkingDays.push(key);
+    else if (value == 4) partWorkingDays.push(key);
+    else nonWorkingDays.push(key);
+});
+console.log("Full Working Days : " + fullWorkingDays);
+console.log("Part Working Days : " + partWorkingDays);
+console.log("NonWorking Days : " + nonWorkingDays);
+
 
 function GetWorkingHrs(empCheck) {
     switch (empCheck) {
